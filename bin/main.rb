27bd5cfc294bda43e3d268423a0b156380c51341
Albertino Francisco <albertino@ua.pt>
2020-05-24 11:35:38 +0100

    Ask for player's game symbol option and set up game symbols for player one and player two.

--- bin/main.rb
+++ bin/main.rb
@@ -15,3 +15,13 @@ until player_two_name.is_a?(String) && !player_two_name.empty?
   player_two_name = gets.chomp
 end
 puts "The Player Two name is #{player_two_name}."
+# Read player's option and set up game symbol
+input_symb1 = ""
+until %w[x X o O].include?(input_symb1)
+  puts "Please choose X or O"
+  input_symb1 = gets.chomp # Read player option.
+end
+puts "#{player_one_name} has chosen #{input_symb1} as his game symbol."
+input_symb2 = input_symb1.upcase == "O" ? "X" : "O"
+puts "#{player_two_name} will play with the game symbol #{input_symb2}"
+puts ""
