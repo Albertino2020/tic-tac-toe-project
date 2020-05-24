27ee59477a63f0f2161c38eb576f9f7fc6f4ab9b
Albertino Francisco <albertino@ua.pt>
2020-05-24 12:07:47 +0100

    Display final results.

--- bin/main.rb
+++ bin/main.rb
@@ -108,6 +108,14 @@ if !game_over
     puts "Match Drawn!!", "\n"
 end
 
-
+# puts "Congratulation #{player_two_name}, you are the winner" if game_over
+# display_board(current_board)
+puts "Recorded Moves:", "\n"
+print board[0], "  |  ", board[1], "  |  ", board[2], "\n"
+print "\n"
+print board[3], "  |  ", board[4], "  |  ", board[5], "\n"
+print "\n"
+print board[6], "  |  ", board[7], "  |  ", board[8], "\n"
+print "\n"
 # display_results()
 # winner()
