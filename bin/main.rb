5fd0bbb533911893d010dd52f008b802e6156de7
Albertino Francisco <albertino@ua.pt>
2020-05-24 11:38:09 +0100

    Initialize Geme Board and display current board.

--- bin/main.rb
+++ bin/main.rb
@@ -25,3 +25,15 @@ puts "#{player_one_name} has chosen #{input_symb1} as his game symbol."
 input_symb2 = input_symb1.upcase == "O" ? "X" : "O"
 puts "#{player_two_name} will play with the game symbol #{input_symb2}"
 puts ""
+# Create player's class instances
+# Initialize game class
+# Initialize Game Board
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Display Current Board
+puts "Current Board:", "\n"
+print board[0], "  |  ", board[1], "  |  ", board[2], "\n"
+print "\n"
+print board[3], "  |  ", board[4], "  |  ", board[5], "\n"
+print "\n"
+print board[6], "  |  ", board[7], "  |  ", board[8], "\n"
+print "\n"
