4823a7f3c7fd558636f6e9ee37a87b3c40935992
Albertino Francisco <albertino@ua.pt>
2020-05-24 21:05:46 +0100

    Update Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,16 @@ class Player
     @name = name
     @symbol = symbol
   end
+
+  def play(option)
+    $board.record(@symbol, option)
+  end
+
+  def check_board()
+    $game_over = $board_check.any? { |opt| opt.all? { |match| match == @symbol } }
+  end
+
+  def switch_player
+    @@played_one, @@played_two = @@played_two, @@played_one
+  end
 end
