e75361b62becef49b4160b746f590c1447ba8799
Albertino Francisco <albertino@ua.pt>
2020-05-24 11:31:56 +0100

    Add players' identification variables, ask players' names and get names inputs from players.

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 puts "Wellcome to the tic-tac-toe geme!"
 # display_instruction()
+# Identify players:
+player_one_name = ""
+player_two_name = ""
+until player_one_name.is_a?(String) && !player_one_name.empty?
+  puts "Player One: What is your name?"
+  player_one_name = gets.chomp
+end
+puts "The Player One name is #{player_one_name}."
+
+until player_two_name.is_a?(String) && !player_two_name.empty?
+  puts "Player Two: What is your name?"
+  player_two_name = gets.chomp
+end
+puts "The Player Two name is #{player_two_name}."
