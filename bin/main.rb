0d3ff19fb0e40b44cabce958bb48977a043674a4
Albertino Francisco <albertino@ua.pt>
2020-05-24 11:49:52 +0100

    Ask player to select from available moves, validate input and repeat if input invalid.

--- bin/main.rb
+++ bin/main.rb
@@ -43,3 +43,35 @@ played_two = true
 count = 0
 check_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 game_over = false
+# Read players' optionn from the keyboard,
+# Validate input,
+# Update Game Board,
+# Display Current Board
+# Update Control Variables
+
+while !game_over && count < 9 && !check_input.empty?
+    # unless check_input.empty?
+    while !played_one && played_two && count < 9
+      puts "#{player_one_name}'s turn Please choose between #{check_input} count: #{count}"
+      player_one_choice = gets.to_i # Read player option from the user
+      next unless check_input.include?(player_one_choice)
+  
+      board[player_one_choice - 1] = input_symb1 # unless !check_input.include?(player_one)
+      check_input.delete(player_one_choice)
+      count += 1
+      played_one = true
+      played_two = false
+    end
+  
+    while !played_two && played_one && count < 9
+      puts "#{player_two_name}'s turn Please choose between #{check_input} count: #{count}"
+      player_two_choice = gets.to_i # Read player option from the user
+      next unless check_input.include?(player_two_choice) # Checks input, validate or reject invalid input
+  
+      board[player_two_choice - 1] = input_symb2 # unless !check_input.include?(player_one)
+      check_input.delete(player_two_choice)
+      count += 1
+      played_one = false
+      played_two = true
+    end
+  end
