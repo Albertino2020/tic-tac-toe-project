79ec8fe29278f812eb8dfde56ff2e0c2065e4bb3
Albertino Francisco <albertino@ua.pt>
2020-05-25 22:10:48 +0100

    Add initialize method to the Board class.

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,9 @@
 class Board
   attr_accessor :player_one, :player_two
+  def initialize(_name1, name2, symb1, symb2, option)
+    @player_one = Player.new(_name1, symb1, option)
+    @player_two = Player.new(name2, symb2, !option)
+  end
+
 
 end
