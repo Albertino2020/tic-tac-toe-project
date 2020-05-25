ae00f424a60875dcf157e278b2313e53b591b3fb
Albertino Francisco <albertino@ua.pt>
2020-05-25 22:03:30 +0100

    Add play method to the Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,22 @@ class Player
     @turn = turn
   end
 
+  def play
+    while @turn
+      puts "#{@name}'s turn: Please choose between #{$check_input}", "\n"
+      @choice = gets.to_i
+      next unless $check_input.include?(@choice)
+      $board.record(@symbol, @choice)
+      $check_input.delete(@choice)
+      $game_over = $board_check.any? { |opt| opt.all? { |match| $current_board[match] == @symbol } }
+      $board.display
+      print $game_over
+      if $game_over
+        @winner = true
+        break
+      end
+      $count += 1
+      @turn = false
+    end
+  end
 end
