49708fb105144223b9a9b098828c369692a7f52c
Albertino Francisco <albertino@ua.pt>
2020-05-25 22:25:02 +0100

    Add player's input variables and define constants.

--- bin/main.rb
+++ bin/main.rb
@@ -3,3 +3,11 @@ require_relative "../lib/board.rb"
 require_relative "../lib/player.rb"
 
 puts "Wellcome to the tic-tac-toe geme!"
+
+player_one_name = ""
+player_two_name = ""
+X = "X".freeze
+O = "O".freeze
+Y = "Y".freeze
+N = "N".freeze
+
