67130a8bcb2c162da0f0dabf3b82d9ec8a537d1a
Albertino Francisco <albertino@ua.pt>
2020-05-25 22:02:12 +0100

    Add initialize method to the Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,10 @@
 class Player
   attr_accessor :name, :symbol, :turn, :turn_one, :turn_two, :choice, :winner
 
+  def initialize(name, symbol, turn)
+    @name = name
+    @symbol = symbol
+    @turn = turn
+  end
+
 end
