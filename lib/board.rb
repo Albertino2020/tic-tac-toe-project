3f10f4999fa3623fd6852d1faccfcbbc1d464312
Albertino Francisco <albertino@ua.pt>
2020-05-25 22:08:27 +0100

    Add lib/board file, create Board class and add attr_accessors to the Board class.

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,4 @@
 class Board
-  def initialize()
-    $current_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  end
+  attr_accessor :player_one, :player_two
 
-  def record(symbol, option)
-    $current_board[option - 1] = symbol
-  end
-
-  def display
-    print $current_board[0], "  |  ", $current_board[1], "  |  ", $current_board[2], "\n"
-    print "\n"
-    print $current_board[3], "  |  ", $current_board[4], "  |  ", $current_board[5], "\n"
-    print "\n"
-    print $current_board[6], "  |  ", $current_board[7], "  |  ", $current_board[8], "\n"
-    print "\n"
-  end
 end
