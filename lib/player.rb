4e0cd3dcfa3614e5f6f522e99c649144b0cde66f
Albertino Francisco <albertino@ua.pt>
2020-05-26 10:43:18 +0100

    Remove global variables, add instance and class variables.

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 # rubocop: disable Style/GlobalVars
 class Player
-  attr_reader :name, :symbol, :turn, :turn_one, :turn_two, :choice, :winner
-  attr_writer :name, :symbol, :turn, :turn_one, :turn_two, :choice, :winner
+  attr_reader :name, :symbol, :turn, :turn_one, :turn_two, :choice, :winner, :game_over, :check_input
+  attr_writer :name, :symbol, :turn, :turn_one, :turn_two, :choice, :winner, :game_over, :check_input
 
   def initialize(name, symbol, turn)
     @name = name
@@ -9,20 +9,38 @@ class Player
     @turn = turn
   end
 
+  def check_input
+    @@check_input
+  end
+
+  def check_input=(value)
+    @@check_input = value
+
+    def board
+      @@board
+    end
+
+    def board=(value)
+      @@board = value
+    end
+  end
+
+  BOARD_CHECK = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
   def play
     while @turn
       @choice = gets.to_i
-      next unless $check_input.include?(@choice)
+      next unless @@check_input.include?(@choice)
 
-      $board.record(@symbol, @choice)
-      $check_input.delete(@choice)
-      $game_over = $board_check.any? { |opt| opt.all? { |match| $current_board[match] == @symbol } }
-      display
-      if $game_over
+      @@board.record(@symbol, @choice)
+      @@check_input.delete(@choice)
+      @game_over = BOARD_CHECK.any? { |opt| opt.all? { |match| @@board.current_board[match] == @symbol } }
+      display(@@board.current_board)
+      if @game_over
         @winner = true
         break
       end
-      $count += 1
+      @@board.count += 1
       @turn = false
     end
   end
