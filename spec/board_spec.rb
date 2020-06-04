fe5f86f9973cd3f37395a88cc6387785002b11ec
Albertino Francisco <albertino@ua.pt>
2020-06-04 13:37:31 +0100

    Add tests for the next_to_play method.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,17 @@
-require './lib/board.rb'
-require './lib/player.rb'
+require "./lib/board.rb"
+require "./lib/player.rb"
 
 RSpec.describe Board do
-  let(:pname1) {'name1'}
-  let(:pname2) {'name2'}
-  let(:symb1) {'X'}
-  let(:symb2) {'O'}
-  let(:option) {true}
-  let(:board) {Board.new(:pname1, :pname2, :symb1, :symb2, :option)}
+  let(:pname1) { "name1" }
+  let(:pname2) { "name2" }
+  let(:symb1) { "X" }
+  let(:symb2) { "O" }
+  let(:option) { true }
+  let(:board) { Board.new(:pname1, :pname2, :symb1, :symb2, :option) }
   let(:current_board) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
-  
 
-  describe '#initialize' do
-    it 'initializes as the board class' do
+  describe "#initialize" do
+    it "initializes as the board class" do
       expect(board.class).to eql(Board)
       expect(board.current_board).to eql(current_board)
       expect(board.player_one.name).to eql(:pname1)
@@ -23,21 +22,27 @@ RSpec.describe Board do
       expect(board.player_two.turn).to eql(!:option)
     end
   end
-  describe '#record' do
+  describe "#record" do
     it "records player'/s move on the board" do
-      board.record('X', 2)
-      expect(board.record('X', 2)).to eql('X')
-      expect(board.current_board).to eql([1, 'X', 3, 4, 5, 6, 7, 8, 9])
-      expect(board.current_board[1]).to eql('X')
+      board.record("X", 2)
+      expect(board.record("X", 2)).to eql("X")
+      expect(board.current_board).to eql([1, "X", 3, 4, 5, 6, 7, 8, 9])
+      expect(board.current_board[1]).to eql("X")
     end
   end
 
-  describe '#next_move' do
-    it 'starts the next players move' do
+  describe "#next_move" do
+    it "starts the next players move" do
       board.player_one.check_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
       board.player_two.check_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
       expect(board.next_move).to eql(board.player_two.play)
     end
   end
-
+  describe "#next_to_play" do
+    it "returns the next to play" do
+      board.player_one.check_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      board.player_two.check_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(board.next_to_play).to eql board.player_one
+    end
+  end
 end
