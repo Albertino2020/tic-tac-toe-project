c87a8d0e7ede2bbe0ce68adcdd096b040401b18a
Albertino Francisco <albertino@ua.pt>
2020-06-04 14:50:59 +0100

    Add test variables to the Player class.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,5 +2,12 @@ require "./lib/board.rb"
 require "./lib/player.rb"
 
 describe Player do
+    let(:name1) { "name1" }
+    let(:symbol1) { "X" }
+    let(:turn1) { true }
+    let(:player) { Player.new(:name1, :symbol1, :turn1) }
+    let(:arr) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+  
+
 
 end
