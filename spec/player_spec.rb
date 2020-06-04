2822ddeb827fcbce7feb086fdd2feb62a71b37a8
Albertino Francisco <albertino@ua.pt>
2020-06-04 14:52:12 +0100

    Add tests for the initialize method.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,6 +8,16 @@ describe Player do
     let(:player) { Player.new(:name1, :symbol1, :turn1) }
     let(:arr) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
   
+    describe "#initialize" do
+        it "Initializes as a Player class" do
+          expect(player.class).to eql(Player)
+        end
+        it "initializes instance variables" do
+          expect(player.name).to eql(:name1)
+          expect(player.symbol).to eql(:symbol1)
+          expect(player.turn).to eql(:turn1)
+        end
+      end
 
 
 end
